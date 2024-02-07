634c19fdff35e8f7f23d0e82168f9ffccbc87dc3
long_burrito <101278892+long-burrito@users.noreply.github.com>
2024-02-06 18:26:25 -0600

    Upload

---
+++ static/home.js
@@ -0,0 +1,15 @@
+function homeLink() {
+    window.location.href = "home.html";
+}
+
+function benefitLink() {
+    window.location.href = "benefits.html";
+}
+
+function applyLink() {
+    window.location.href = "templates/apply.html";
+}
+
+function loginLink() {
+    window.location.href - "login.html";
+}
